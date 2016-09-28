05a907c0689fd863d1dc5c1823b90dcff3ce0d93
Jarosław Frydrych <jarek.frydrych@gmail.com>
2016-09-28 23:40:15 +0200

    server

--- app.js
+++ app.js
@@ -61,8 +61,7 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end("hello world!\n");
+
+http.listen(3000, function () {
+  console.log('listening on *:3000');
 });
-server.listen(3000);
