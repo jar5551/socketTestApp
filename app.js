4fb372c67770e25d61dd85d96679a758db0c5503
Jarosław Frydrych <jarek.frydrych@gmail.com>
2016-09-28 23:40:54 +0200

    server

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var http = require('http');
 
 var app = express();
+var http = require('http').Server(app);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
